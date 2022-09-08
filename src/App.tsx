7c292042ee0f18b323ab48187262987b4e2d68e0
Denis Karpenko <denis_swd@mail.ru>
2022-09-08 21:11:17 +0200

    installed prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,36 @@
-import React, {FC, useCallback, useState} from 'react';
+import React, { FC, useCallback, useState } from 'react'
 import s from './App.module.scss'
-import {Button} from "antd";
-import {PoweroffOutlined} from '@ant-design/icons';
+import { Button } from 'antd'
+import { PoweroffOutlined } from '@ant-design/icons'
 import initialize from '../src/assets/INITIALIZE_1_HQ.json'
-import Lottie from "lottie-react";
-import {delay} from "./utilits/delay";
-import {MessageBox} from "./components/messageBox/MessageBox";
+import Lottie from 'lottie-react'
+import { delay } from './utilits/delay'
+import { MessageBox } from './components/messageBox/MessageBox'
 
 export const App: FC = () => {
   const [displayButton, setDisplayButton] = useState(true)
   const [displayMessage, setDisplayMessage] = useState(false)
 
+ // use hook for optimize
   const startLoading = useCallback(async () => {
+    //hide button and display preloader
     setDisplayButton(false)
+    //wait 3 seconds
     await delay(3000)
+    //hide preloader and display message
     setDisplayMessage(true)
   }, [])
 
   return (
     <div className={s.app}>
       {displayButton && <Button size={'large'}
-                                type="primary"
-                                icon={<PoweroffOutlined/>}
+                                type='primary'
+                                icon={<PoweroffOutlined />}
                                 onClick={startLoading}>Click me!</Button>}
       {!displayButton &&
         !displayMessage &&
-          <Lottie animationData={initialize} loop className={s.preloader}/>}
-      {displayMessage && <MessageBox message={'Привет!\nЧем могу помочь?'}/>}
+        <Lottie animationData={initialize} loop className={s.preloader} />}
+      {displayMessage && <MessageBox message={'Привет!\nЧем могу помочь?'} />}
     </div>
-  );
+  )
 }
--- src/components/messageBox/MessageBox.tsx
+++ src/components/messageBox/MessageBox.tsx
@@ -1,12 +1,12 @@
-import {FC} from "react";
+import { FC } from 'react'
 import s from './MessageBox.module.scss'
 
 type MessageBoxProps = {
   message: string
 }
 
-export const MessageBox:FC<MessageBoxProps> =({message})=>{
-  return(
+export const MessageBox: FC<MessageBoxProps> = ({ message }) => {
+  return (
     <p className={s.messageBox}>
       {message}
     </p>
