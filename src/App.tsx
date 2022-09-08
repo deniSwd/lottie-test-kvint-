18bf83c1042c4176b60660b055c5e59a4e9cd33d
Denis Karpenko <denis_swd@mail.ru>
2022-09-08 15:01:59 +0200

    installed lottie-react, added utlit for delay

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
 import React, {FC} from 'react';
 import s from './App.module.scss'
+import {Button} from "antd";
+import { PoweroffOutlined } from '@ant-design/icons';
+import initialize from '../src/assets/INITIALIZE_1_HQ.json'
+import {useLottie} from "lottie-react";
 
 export const App:FC = () => {
+
+  const options = {
+    animationData: initialize,
+    loop: true,
+  }
+  const {View}  = useLottie(options)
+
+
   return (
     <div className={s.app}>
-      <button>Click</button>
+      <Button
+        size={'large'}
+        type="primary"
+        icon={<PoweroffOutlined />}
+        onClick={() => console.log('!') }
+      >
+        Click me!
+      </Button>
+      <div>
+        { View }
+      </div>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import 'antd/dist/antd.css'
 import reportWebVitals from './reportWebVitals';
 import { App } from "./App";
 
