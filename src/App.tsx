d4d632ffc19490d44803136d5f0fc0af5aaee195
Denis Karpenko <denis_swd@mail.ru>
2022-09-08 23:34:56 +0200

    trying to add an animation to a message, changed README

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,27 @@ import React, { FC, useCallback, useState } from 'react'
 import s from './App.module.scss'
 import { Button } from 'antd'
 import { PoweroffOutlined } from '@ant-design/icons'
-import initialize from '../src/assets/INITIALIZE_1_HQ.json'
+import initialize from './assets/INITIALIZE_1_HQ.json'
 import Lottie from 'lottie-react'
 import { delay } from './utilits/delay'
 import { MessageBox } from './components/messageBox/MessageBox'
+import { Transition } from 'react-transition-group'
+
+const duration = 800
+
+const transitionStyles = {
+  entering: { opacity: 1 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 },
+  unmounted: { opacity: 0 }
+}
 
 export const App: FC = () => {
   const [displayButton, setDisplayButton] = useState(true)
   const [displayMessage, setDisplayMessage] = useState(false)
 
- // use hook for optimize
+  // use hook for optimize
   const startLoading = useCallback(async () => {
     //hide button and display preloader
     setDisplayButton(false)
@@ -30,7 +41,10 @@ export const App: FC = () => {
       {!displayButton &&
         !displayMessage &&
         <Lottie animationData={initialize} loop className={s.preloader} />}
-      {displayMessage && <MessageBox message={'Привет!\nЧем могу помочь?'} />}
+      <Transition timeout={duration} in={displayMessage}>
+        {state => <MessageBox style={transitionStyles[state]}
+                              message={'Привет!\nЧем могу помочь?'} />}
+      </Transition>
     </div>
   )
 }
--- src/components/messageBox/MessageBox.tsx
+++ src/components/messageBox/MessageBox.tsx
@@ -1,13 +1,14 @@
-import { FC } from 'react'
+import { CSSProperties, FC } from 'react'
 import s from './MessageBox.module.scss'
 
 type MessageBoxProps = {
+  style: CSSProperties
   message: string
 }
 
-export const MessageBox: FC<MessageBoxProps> = ({ message }) => {
+export const MessageBox: FC<MessageBoxProps> = ({ message, style }) => {
   return (
-    <p className={s.messageBox}>
+    <p className={s.messageBox} style={style}>
       {message}
     </p>
   )
